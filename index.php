786d4837f4f03e4f8734b2886f1d85de17bb912d
KanyaratRungrueang07 <86422790+KanyaratRungrueang07@users.noreply.github.com>
2021-12-21 07:57:46 +0700

    Update index.php

--- index.php
+++ index.php
@@ -16,178 +16,135 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <!----<link rel="stylesheet" href="fa/css/font-awesome.min.css">---->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css">
-
+    <link rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css"
+        integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
 </head>
+<style>
+    .h1{
+        background-color: aquamarine;
+    }
+    .container{
+        background-color: burlywood;
+    }
+    .body{
+        background-color: burlywood;
+    }
 
-<body>
-    <div class="container">
+</style>
 
-        <div>
+<body class="body">
+    <br>
+    <h4 style="text-align: center;">Restaur Yummy</h4>
+    <br>
+
             <table class="table">
                 <thead>
                     <tr>
 
                         <td colspan=2>
                             <input type="hidden" name="" id="txtRID" value="0" />
-                            <input type="password" class="form-control" id="txtUnique" placeholder="Unique Name" />
+                            <input type="password" class="form-control" id="txtnameras" placeholder="ชื่อร้าน" />
 
                         </td>
-                        <td><input type="text" class="form-control" id="txtName" placeholder="Name" /></td>
-                        <td><input type="text" class="form-control" id="txtOwner" placeholder="Owner" /></td>
-                        <td><input type="text" class="form-control" id="txtWebSite" placeholder="WebSite" /></td>
-                        <td><button class="btn btn-success" id="btnAdd"> <i class="far fa-plus"></i>  Add </button></td>
-                        <td><button class="btn btn-danger" id="btnDelete"> <i class="fa fa-plus-circle"
-                                    aria-hidden="true"></i> Delete </button></td>
+                        <td><input type="text" class="form-control" id="txtRID" placeholder="รหัสอาหาร" /></td>
+                        <td><input type="text" class="form-control" id="txtname" placeholder="ชื่ออาหาร" /></td>
+                        <td><input type="text" class="form-control" id="pricefood" placeholder="ราคา" /></td>
+                        <td><button class="btn btn-success" id="btnAdd"> 
+                            <i class="fa fa-plus-circle" aria-hidden="true"></i> Add </button></td>
+                        <td><button class="btn btn-danger" id="btnClear>
+                            <i class="fa fa-trash" aria-hidden="true"></i> Delete </button></td>
+                            <td><button class="btn btn-warning" id="btnUpdate">
+                                <i class="fa fa-upload" aria-hidden="true"></i> Update </button></td>
 
                     </tr>
-                    <tr>
-                        <th></th>
-                        <th>ID</th>
-                        <th>Name</th>
-                        <th>Owner</th>
-                        <th>Website</th>
-                        <th>Status</th>
-                    </tr>
+                    <div class="container">
+                        <table id="tblAll" class="table table-striped" style="margin-top:23px">
+                            <thead>
+                                <tr>
+                                    <th>ชื่อร้าน </th>
+                                    <th>ID อาหาร </th>
+                                    <th>ชื่ออาหาร </th>
+                                    <th>ราคา </th>
+                
+                                </tr>
+                            </thead>
+                        </table>
+                        <br/>
+                    </div>
                 </thead>
                 <tbody id="tblRestaurants"></tbody>
             </table>
-        </div>
-
-
-        <select id="optCate" class="form-control">
-            <option value="1"> Hello </option>
-        </select>
-    </div>
-
-
 
 
 
 </body>
 <script>
-    function loadCate() {
-        var url = "https://cjundang.me/api/category/gets";
-        $.getJSON(url)
-            .done((data) => {
-                var line = "";
-                $.each(data, (k, v) => {
-                    line += "<option value='" + v.cid + "'> " + v.categories + "</option>";
-                });
-                $("#optCate").empty();
-                $("#optCate").append(line);
-
-            })
-
+    $(function () {
+        $('#btnAdd').on('click', function () {
+            var txtnameras ,txtRID, txtname, pricefood;
+            txtnameras = $("#txtnameras").val();
+            txtname = $("#txtname").val();
+            txtRID = $("#txtRID").val();
+            pricefood = $("#pricefood").val();
+            var edit = "<a class='edit' href='JavaScript:void(0);' >Edit</a>";
+            var del = "<a class='delete' href='JavaScript:void(0);'>Delete</a>";
+            if (txtRID == "" || pricefood == "" || txtnameras == " ") {
+                alert("กรุณากรอกข้อมูล!");
+            } else {
+                var table = "<tr><td>" + txtname + "</td><td>" + txtnameras + "</td><td>" + txtRID + "</td><td>" + pricefood + "</td><td>" + edit + "</td><td>" + del + "</td></tr>";
+                $("#tblAll").append(table);
+            }
+            txtname = $("#txtname").val("");
+            txtnameras = $("#txtnameras").val();
+            txtRID = $("#txtRID").val("");
+            pricefood = $("#pricefood").val("");
+            Clear();
+        });
+        $('#btnUpdate').on('click', function () {
+            var txtnameras ,txtRID, pricefood, txtname;
+            txtnameras = $("#txtnameras").val();
+            txtname = $("#txtname").val();
+            txtRID = $("#txtRID").val();
+            pricefood = $("#pricefood").val();
+            $('#tblAll tbody tr').val().find('td').eq(0).html(txtnameras);
+            $('#tblAll tbody tr').val().find('td').eq(1).html(txtRID);
+            $('#tblAll tbody tr').val().find('td').eq(2).html(txtname);
+            $('#tblAll tbody tr').val().find('td').eq(3).html(pricefood);
+            $('#btnAdd').show();
+            $('#btnUpdate').hide();
+            Clear();
+        });
+        $("#tblAll").on("click", ".delete", function (e) {
+            if (confirm("ยืนยันการลบ")) {
+                $(this).closest('tr').remove();
+            } else {
+                e.preventDefault();
+            }
+        });
+        $('#btnClear').on('click', function () {
+            Clear();
+        });
+        $("#tblAll").on("click",".edit", function (e) {
+            var row = $(this).closest('tr');
+            $('#hfRowIndex').val($(row).index());
+            var td = $(row).find("td");
+            $('#txtnameras').val($(td).eq(1).html());
+            $('#txtname').val($(td).eq(0).html());
+            $('#txtRID').val($(td).eq(2).html());
+            $('#pricefood').val($(td).eq(3).html());
+            $('#btnAdd').hide();
+            $('#btnUpdate').show();
+        });
+    });
+    function Clear() {
+        $("#txtnameras").val("");
+        $("#txtname").val("");
+        $("#txtRID").val("");
+        $("#pricefood").val("");
+        $("#hfRowIndex").val("");
     }
-    function loadRestaurants() {
-        var url = "https://cjundang.me/api/restaurants/gets";
-        $.getJSON(url).done((data) => {
-            var line = "";
-            console.log(data);
-            $.each(data, (key, item) => {
-                line += "<tr>";
-                line += "<td> <a class='btn btn-danger btn-sm' href='#' onClick='deleRestaurant(" + item.rid + ")'><i class='fa fa-trash' aria-hidden='true'></i> dele </a> ";
-                line += "<a class='btn btn-primary  btn-sm' href='#' onClick='loadARestaurant(" + item.rid + ")' > <i class='fa fa-pencil-square-o' aria-hidden='true'></i> edit </a></td>";
-                line += "<td>" + item.rid + "</td>";
-                line += "<td>" + item.name + "</td>";
-                line += "<td>" + item.owner_name + "</td>";
-                line += "<td>  <a href='" + item.website + "'> Link </a></td>";
-                line += "<td>";
-                line += "Opened </td>";
-                line += "</tr>";
-                console.log(line);
-
-
-            });
-            $("#tblRestaurants").empty();
-            $("#tblRestaurants").append(line);
-
-        })
-            .fail((xhr, status, error) => {
-
-            });
-    }
-
-    function loadARestaurant(id) {
-        var url = "https://cjundang.me/api/restaurants/get/" + id;
-        $.getJSON(url).done((data) => {
-            console.log(data);
-            var item = data[0];
-            $("#txtRID").val(item.rid);
-            $("#txtName").val(item.name);
-            $("#txtOwner").val(item.owner_name);
-            $("#txtWebSite").val(item.website);
-
-
-        })
-            .fail((xhr, status, error) => {
-
-            });
-    }
-
-    function deleRestaurant(id) {
-
-        var unique_name = prompt("confirm to delete with Uniqe Name");
-        if (unique_name != null) {
-            var data = new Object();
-            data.unique_name = unique_name;
-            data.rstatus = 1;
-            var url = "https://cjundang.me/api/restaurants/status";
-            $.post(
-                url,
-                { "data": JSON.stringify(data) },
-                (data, status) => {
-                    console.log(data + " " + status);
-                    loadRestaurants();
-                }
-            )
-        }
-
-
-    }
-
-    function addRestaurant() {
-        var data = new Object();
-        data.unique_name = $("#txtUnique").val();
-        data.name = $("#txtName").val();
-        data.owner_name = $("#txtOwner").val();
-        data.website = $("#txtWebSite").val();
-        console.log(JSON.stringify(data));
-        var rid = parseInt($("#txtRID").val());
-
-        if (rid == 0) {  // add
-            var url = "https://cjundang.me/api/restaurants/add";
-            $.post(
-                url,
-                {
-                    "data": JSON.stringify(data)
-                },
-                (data, status) => {
-                    console.log(data + " " + status);
-                    loadRestaurants();
-                }
-            );
-        } else {
-            var url = "https://cjundang.me/api/restaurants/update";
-            $.post(
-                url,
-                {
-                    "data": JSON.stringify(data)
-                },
-                (data, status) => {
-                    console.log(data + " " + status);
-                    loadRestaurants();
-                }
-            );
-        }
-    }
-
-    $(() => {
-        loadRestaurants();
-        loadCate();
-        $("#btnAdd").click(addRestaurant);
-    })
 </script>
 
 </html>
